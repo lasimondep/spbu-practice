da18203353afb4f6e36b62c3c77598544efab565
Simon Lagutin <lasimondep@inbox.ru>
2017-09-27 20:41:16 +0300

    Some changes in problem 1

--- 21.09.2017/problem1.cpp
+++ 21.09.2017/problem1.cpp
@@ -3,14 +3,14 @@
 const int MAXN(100);
 
 int main() {
- 	int n, m, k(0), *a = new int [MAXN * MAXN];
+ 	int n, m, *a = new int [MAXN * MAXN];
  	scanf("%d%d", &n, &m);
  	for (int i(0); i < n; ++i)
  		for (int j(0); j < m; ++j)
- 			a[i * m + j] = ++k;
+ 			scanf("%d", a + i * m + j);
  	for (int i(0); i < n; ++i) {
 		for (int j(0); j < m; ++j)
-			printf(k > 0xf ? k > 0xff ? k > 0xfff ? "%4x " : "%3x " : "%2x " : "%x ", a[i * m + j]);
+			printf("%d ", a[i * m + j]);
 		putchar('\n');
 	}
 	return 0;
